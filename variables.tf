9c363bd8b2dc1bb74301175a663d2ea3e4bb8f07
Venkat <venkat26072003@gmail.com>
2024-09-23 16:28:18 +0000

    trial1

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "vpc_name" {
 variable "instance_count" {
   type        = number
   description = "Number of EC2 instances"
-  default     = 2
+  default     = 0
 }
 
 variable "db_instance_class" {
