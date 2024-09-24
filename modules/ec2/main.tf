0e3d1f67ded155ba21200420359f7f731318d323
Venkat <venkat26072003@gmail.com>
2024-09-24 17:14:31 +0000

    updated foe chef client

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,9 +1,24 @@
 resource "aws_instance" "app" {
   count         = var.instance_count
-  ami           = "ami-08718895af4dfa033"  # Replace with your AMI ID
+  ami           = "ami-0522ab6e1ddcc7055"  # Replace with your AMI ID
   instance_type = "t2.micro"
   subnet_id     = element(var.subnet_ids, count.index)
   vpc_security_group_ids = [var.security_group]
+
+  key_name = "demo"
+
+  provisioner "remote-exec" {
+    inline = [
+      "curl -L https://omnitruck.chef.io/install.sh | sudo bash"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/demo.pem")
+      host        = self.public_ip
+    }
+  }
 
   tags = {
     Name = "App-${count.index}"
