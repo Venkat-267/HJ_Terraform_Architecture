e8ac83219e3efa4634b7d2fbcb66fa9ab3c10f13
Venkat <venkat26072003@gmail.com>
2024-09-25 17:40:54 +0000

    updates

--- .chef/config.rb
+++ .chef/config.rb
@@ -4,6 +4,6 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "admin134"
-# client_key               "#{current_dir}/admin134.pem"
+client_key               "#{current_dir}/admin134.pem"
 chef_server_url          "https://ip-172-31-36-31.ap-south-1.compute.internal/organizations/orgs"
 cookbook_path            ["#{current_dir}/../cookbooks"]
