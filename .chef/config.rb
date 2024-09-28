eb2f3ded6514067b97431c6a4b1e8eb0ecb3a6d6
Venkat <venkat26072003@gmail.com>
2024-09-28 15:55:19 +0000

    trial14

---
+++ .chef/config.rb
@@ -0,0 +1,5 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                'node_ec2'  
+chef_server_url          "https://ip-172-31-36-31.ap-south-1.compute.internal/organizations/orgs"
